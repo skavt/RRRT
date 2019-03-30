f169d71eb859ea30754018fc68017c05abbf1f56
SaloMe KavtaRadze <44123889+skavt@users.noreply.github.com>
2019-03-30 21:51:11 +0400

    Create Template.php

---
+++ src/H3/Template.php
@@ -0,0 +1,47 @@
+<?php
+
+class Template
+{
+    private $tags = [];
+
+    private $template;
+
+    public function render($templateFile)
+    {
+        $this->template = $this->getFile($templateFile);
+
+        if(!$this->template) {
+            return "Error! Can't load the template file $templateFile";
+        }
+        $this->replaceTags();
+
+        echo $this->template;
+    }
+
+    public function set($tag, $value)
+    {
+        $this->tags[$tag] = $value;
+    }
+
+    public function getFile($file)
+    {
+        if(file_exists($file))
+        {
+            $file = file_get_contents($file);
+            return $file;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private function replaceTags()
+    {
+        foreach ($this->tags as $tag => $value) {
+            $this->template = str_replace('{'.$tag.'}', $value, $this->template);
+        }
+
+        return true;
+    }
+}
