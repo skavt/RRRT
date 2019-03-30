35cf69f21281b4fef546472b7a4735cc881de052
SaloMe KavtaRadze <44123889+skavt@users.noreply.github.com>
2019-03-30 21:50:43 +0400

    Create Router.php

---
+++ src/H2/Router.php
@@ -0,0 +1,35 @@
+<?php
+
+include_once 'Controller.php';
+
+class Router
+{
+	private $request;
+
+	private $controller;
+	public function __construct($request)
+	{
+		$this->request = $request;
+		$this->controller = new Controller();
+	}
+
+	public function get($route, $method)
+	{
+
+		$uri = rtrim($route, "/" );
+
+		$uri = explode("/", $uri);
+
+		if($uri[1] == trim($route, "/"))
+		{
+			array_shift($uri);
+			$args = $uri;
+
+			$this->controller->$method();
+		}
+
+	}
+	
+}
+
+?>
