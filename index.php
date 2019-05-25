9dc3ae53a8271fcf9064b635bb34f5551e69a04c
SaloMe KavtaRadze <44123889+skavt@users.noreply.github.com>
2019-05-25 17:41:16 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
 <?php
 
-include_once 'src/H1/Request.php';
-include_once 'src/H1/Response.php';
-include_once 'src/H2/Router.php';
+use src\H1\Request;
+use src\H1\Response;
+use src\H2\Router;
+
+// include_once 'src/H1/Request.php';
+// include_once 'src/H1/Response.php';
+// include_once 'src/H2/Router.php';
+
+spl_autoload_register(function($class) {
+    $path = str_replace('\\', '/', $class.'.php');
+    if (file_exists($path)) {
+        require $path;
+    }
+});
 
 $router = new Router(new Request);
 
 $router->get('/new', 'main');
-
-?>
